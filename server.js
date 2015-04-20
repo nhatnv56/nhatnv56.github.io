b4dd14de2b9a21914222d958e2dbf9e37e383e08
Nhat <nhatnv_56@vnu.edu.vn>
2015-04-20 11:09:46 -0400

    :'(

--- server.js
+++ server.js
@@ -1,7 +1,64 @@
 var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var faker = require('faker');
 var app = express();
+
+app.set('views', __dirname + '/views');
+app.engine('.html',require('ejs').__express);
+app.set('view engine','html');
+
+app.use(session({secret: 'ssshhhhh'}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname+'/public'));
-app.get('/', function(req,res){
-    res.sendfile('index.html');
-})
-app.listen(8080);
+
+var sess;
+//------------faker data-------
+// var paragraphs = [];
+// var sentence = [];
+div = [];
+for(var i = 12; i>0; i--){
+    div.push({
+        comment: faker.lorem.paragraphs(),
+        title: faker.lorem.sentence()
+    });
+}
+//------------faker data-------
+app.get('/',function(req,res){
+    sess=req.session;
+    if(sess.email){
+        res.redirect('/home');
+    }else{
+        res.render('index');
+    }
+});
+
+app.post('/login',function(req,res){
+    sess=req.session;
+    sess.email=req.body.email;
+    res.end('done');
+});
+
+app.get('/home',function(req,res){
+    sess=req.session;
+    if(sess.email){
+        res.render('home',{email: sess.email, div: div});
+    }else{
+        res.write('<h1>Please login first.</h1>');
+        res.end('<a href="/">Login</a>');
+    }
+});
+
+app.get('/logout',function(req,res){
+    req.session.destroy(function(err){
+        if(err)
+            console.log(err);
+        else
+            res.redirect('/');
+    });
+
+});
+app.listen(3000,function(){
+    console.log("App Started on PORT 3000");
+});
