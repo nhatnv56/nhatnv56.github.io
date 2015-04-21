a0aac7b954aafc87114b1d167d361e5fc9719a7c
Nhat <nhatnv_56@vnu.edu.vn>
2015-04-21 00:51:08 -0400

    connected mongoose

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var faker = require('faker');
 var app = express();
+var Schema = mongoose.Schema;
 
 app.set('views', __dirname + '/views');
 app.engine('.html',require('ejs').__express);
@@ -12,12 +14,18 @@ app.use(session({secret: 'ssshhhhh'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname+'/public'));
+mongoose.connect('mongodb://localhost/test');
+
+var UserSchema = new Schema({
+    email:{type: String, required: true},
+    password:{type: String, required: true},
+});
+var User = mongoose.model('users', UserSchema);
 
 var sess;
+
 //------------faker data-------
-// var paragraphs = [];
-// var sentence = [];
-div = [];
+var div = [];
 for(var i = 12; i>0; i--){
     div.push({
         comment: faker.lorem.paragraphs(),
@@ -25,6 +33,7 @@ for(var i = 12; i>0; i--){
     });
 }
 //------------faker data-------
+
 app.get('/',function(req,res){
     sess=req.session;
     if(sess.email){
@@ -35,9 +44,22 @@ app.get('/',function(req,res){
 });
 
 app.post('/login',function(req,res){
-    sess=req.session;
-    sess.email=req.body.email;
-    res.end('done');
+    User.findOne({
+        email: req.body.email,
+        password: req.body.pass
+    },function(err, doc) {
+        if(err){
+            console.log(err);
+        }else{
+            if(doc===null){
+                console.log('ko tim thay gi');
+                res.end('error');
+            }
+            sess=req.session;
+            sess.email=req.body.email;
+            res.end('done');
+        }
+    });
 });
 
 app.get('/home',function(req,res){
@@ -45,8 +67,7 @@ app.get('/home',function(req,res){
     if(sess.email){
         res.render('home',{email: sess.email, div: div});
     }else{
-        res.write('<h1>Please login first.</h1>');
-        res.end('<a href="/">Login</a>');
+        res.redirect('/');
     }
 });
 app.post('/home', function(req, res) {
@@ -66,6 +87,23 @@ app.get('/logout',function(req,res){
             res.redirect('/');
     });
 
+});
+app.post('/create',function (req,res) {
+    console.log(req.body.email);
+    console.log(req.body.pass);
+    // res.send('done');
+    User.create({
+        email: req.body.email,
+        password: req.body.pass
+    },function (err, doc) {
+        if(err){
+            console.log(err);
+            res.end('error');
+        }else{
+            console.log('added: \n' + doc);
+            res.end('done');
+        }
+    });
 });
 app.listen(3000,function(){
     console.log("App Started on PORT 3000");
